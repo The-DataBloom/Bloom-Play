6cd3f5532e0edfa3b73102d9da4ee04f62cd57e1
Data Bloom <83243831+The-DataBloom@users.noreply.github.com>
2026-08-03 12:34:10 +0330

    Delete App directory

--- App/Api/state.py
+++
@@ -1,3 +0,0 @@
-import time
-
-last_client_seen = time.time()
--- App/Utils/qr.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-
-
-def create_qr(url, path="qr.png"):
-    img = qrcode.make(url)
-    img.save(path)
-
-    return path
